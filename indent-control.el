383e97d6792445d7cea10a838da3d30d415a5fe4
JenChieh <jcs090218@gmail.com>
2020-12-15 20:32:29 +0800

    Add comment.

--- indent-control.el
+++ indent-control.el
@@ -251,6 +251,7 @@
   "Keep the tab width the same as last time modified."
   (indent-control-set-indent-level-by-mode (indent-control--get-indent-level-record-by-mode)))
 
+;; NOTE: Initial the indent level once after module is loaded.
 (indent-control-continue-with-tab-width-record)
 
 (provide 'indent-control)
