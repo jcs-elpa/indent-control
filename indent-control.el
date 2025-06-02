079e7a08345c0abd1d20eadd668c4056adfdb2c6
JenChieh <jcs090218@gmail.com>
2025-06-02 04:00:45 -0700

    Bump version for next release

--- indent-control.el
+++ indent-control.el
@@ -5,7 +5,7 @@
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; URL: https://github.com/jcs-elpa/indent-control
-;; Version: 0.4.0
+;; Version: 0.4.1
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: convenience control indent tab generic level
 
